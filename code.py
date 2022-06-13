e89141a685ec3603bb6ba5bf07d3b822cbf7f81b
Ferdous Sediqi <ferdous.sediqi@stu.ocsb.ca>
2022-06-13 14:55:13 -0400

    CPT day 4 and 5

--- code.py
+++ code.py
@@ -6,6 +6,9 @@ import ugame
 # import stage
 import stage 
 
+# import constant file
+import constants
+
 # function for main scene of the game
 def game_scene():
     
@@ -16,18 +19,44 @@ def game_scene():
 
 
     # set background image
-    background = stage.Grid(image_bank_background, 10, 8)
-    ship = stage.Sprite(image_bank_sprite, 5, 75, 66)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    ship = stage.Sprite(image_bank_sprite, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
     # create stage for the background
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
     # set layers for all sprites
     game.layers = [ship] + [background]
     game.render_block()
 
     # looping the scene
     while True:
-        pass
+        # get user response from pressing buttons and change the x and y axis
+        keys = ugame.buttons.get_pressed()
+        if keys & ugame.K_X:
+            pass
+        if keys & ugame.K_O:
+            pass
+        if keys & ugame.K_START:
+            pass
+        if keys & ugame.K_SELECT:
+            pass
+        if keys & ugame.K_RIGHT:
+        # if statment to not let ship got out of screen
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+            else:
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
+        if keys & ugame.K_LEFT:
+        # if statment to not let ship got out of screen
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+            else:
+                ship.move(0, ship.y)
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
+        # render gqme
         game.render_sprites([ship])
         game.tick()
 
---
+++ constants.py
@@ -0,0 +1,16 @@
+#
+#created by : Ferdous Sediqi
+# created on : June 2022
+
+# pybadge screen size is 160x128 and sprites are 16x16
+
+# constants 
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# library for button states
